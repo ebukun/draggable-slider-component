39055cf2fe92a7ad7bf15182d8af57c7fbee9aa8
Ibukun Okunfolami <ibukunokunfolami@gmail.com>
2022-02-17 01:15:50 +0100

    style clean

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Slider from "./Slider";
 export default function App() {
   return (
     <div className="App">
-      <Slider />
+      <div className="App-container">
+        <Slider />
+      </div>
     </div>
   );
 }
